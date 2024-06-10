bf350cdbd8227cdb0dc1f7887a908b2acc093f7d
AleCalz <calzadaloyaalejandra@gmail.com>
2024-06-10 16:46:27 -0600

    Add dependencie Cors

--- src/server.js
+++ src/server.js
@@ -2,8 +2,10 @@ const express = require('express')
 const userRouter = require('./routes/user.router')
 const postRouter = require('./routes/post.router')
 const authRouter = require('./routes/auth.router')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 app.use('/user', userRouter)
