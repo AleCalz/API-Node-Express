3720ccb2d00b5c35e9d0a0f7217698ed6faa87f1
AleCalz <calzadaloyaalejandra@gmail.com>
2024-09-06 19:44:31 -0600

    add Swagger

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ require('dotenv').config()
 const server = require('./src/server')
 const PORT = process.env.PORT || 8080
 const db = require('./src/lib/mongooseDb')
+const { swaggerDocs: V1SwaggerDocs } = require('./src/swagger')
 
 db.connect()
   .then(() => {
     console.log('DB Connected!')
     server.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`)
+      V1SwaggerDocs(server, PORT)
     })
   })
   .catch((error) => console.log(`DB connection error: ${error}`))
---
+++ src/swagger.js
@@ -0,0 +1,41 @@
+const swaggerJSDoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
+
+// Metadata info  API
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API-NodeJS - Manejo de Ususarios',
+      version: '1.0.0',
+      description: 'Retos de Backend',
+      license: {
+        name: 'MIT',
+        url: 'https://en.wikipedia.org/wiki/MIT_License'
+      }
+    },
+    host: 'localhost:3000',
+    basePath: '/',
+    schemes: ['http'],
+    consumes: ['application/json'],
+    produces: ['application/json']
+  },
+  apis: ['./src/routes/*.js']
+}
+
+// Doc en JSON
+
+const swaggerSpec = swaggerJSDoc(options)
+
+// fconfigurar nuestra documentación
+const swaggerDocs = (app, port) => {
+  app.use('/v1/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+  app.get('/v1/api-docs.json', (req, res) => {
+    res.setHeader('Content-Type', 'application/json')
+    res.send(swaggerSpec)
+  })
+
+  console.log(`Docs disponibles en http://localhost:${port}/v1/api-docs`)
+}
+
+module.exports = { swaggerDocs }
