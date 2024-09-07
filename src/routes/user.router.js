effd03c4ef652a18adb1eae68e28a1d866e7087a
AleCalz <calzadaloyaalejandra@gmail.com>
2024-09-06 21:52:23 -0600

    add user schema to swagger

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -2,6 +2,52 @@ const express = require('express')
 const userUseCases = require('../usecases/user.usecases')
 const router = express.Router()
 
+/**
+ * @openapi
+ * /user/{id}:
+ *  get:
+ *    description: Get user by id
+ *    tags:
+ *      - user
+ *    parameters:
+ *      - name: id
+ *        in: path
+ *        description: User id
+ *        required: true
+ *        type: string
+ *    responses:
+ *      200:
+ *        description: Success
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                success:
+ *                  type: boolean
+ *                data:
+ *                  type: object
+ *                  properties:
+ *                    userFound:
+ *                      type: object
+ *                      properties:
+ *                        id:
+ *                          type: string
+ *                        name:
+ *                          type: string
+ *                        profilePic:
+ *                          type: string
+ *                        email:
+ *                        password:
+ *                          type: string
+ *                        createdAt:
+ *                          type: date
+ *                        updatedAt:
+ *                          type: date
+ *      500:
+ *        description: Internal Server Error
+ *
+ */
 router.get('/:id', async (req, res) => {
   try {
     const { id } = req.params
@@ -19,6 +65,59 @@ router.get('/:id', async (req, res) => {
   }
 })
 
+/**
+ * @openapi
+ * /user:
+ *  post:
+ *    description: Add new user
+ *    tags:
+ *      - user
+ *    requestBody:
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              name:
+ *                type: string
+ *              email:
+ *                type: string
+ *              password:
+ *                type: string
+ *              profilePic:
+ *                type: string
+ *    responses:
+ *      200:
+ *        description: Success
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                success:
+ *                  type: boolean
+ *                data:
+ *                  type: object
+ *                  properties:
+ *                    newUser:
+ *                      type: object
+ *                      properties:
+ *                        id:
+ *                          type: string
+ *                        name:
+ *                          type: string
+ *                        email:
+ *                          type: string
+ *                        password:
+ *                          type: string
+ *                        createdAt:
+ *                          type: date
+ *                        updatedAt:
+ *                          type: date
+ *      500:
+ *        description: 'Error: Internal Server Error'
+ *
+ */
 router.post('/', async (req, res) => {
   try {
     const { body } = req
@@ -36,6 +135,46 @@ router.post('/', async (req, res) => {
   }
 })
 
+/**
+ * @openapi
+ * /user:
+ *  get:
+ *    description: Get all users
+ *    tags:
+ *      - users
+ *    responses:
+ *      200:
+ *        description: Success
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                status:
+ *                  type: string
+ *                  example:
+ *                data:
+ *                  type: object
+ *                  properties:
+ *                    allUsers:
+ *                      type: array
+ *                      items:
+ *                        type: object
+ *                        properties:
+ *                          id:
+ *                            type: string
+ *                          name:
+ *                            type: string
+ *                          email:
+ *                            type: string
+ *                          password:
+ *                            type: string
+ *                          createdAt:
+ *                            type: date
+ *                          updatedAt:
+ *                            type: date
+ *
+ */
 router.get('/', async (req, res) => {
   try {
     const allUsers = await userUseCases.getAll()
@@ -52,6 +191,27 @@ router.get('/', async (req, res) => {
   }
 })
 
+/**
+ * @openapi
+ * /user/{id}:
+ *  delete:
+ *    description: Delete user by id
+ *    tags:
+ *      - user
+ *    parameters:
+ *      - name: id
+ *        in: path
+ *        description: User id
+ *        required: true
+ *        type: string
+ *    responses:
+ *      200:
+ *        description: Success
+ *      500:
+ *        description: Internal Server Error
+ *
+ *
+ */
 router.delete('/:id', async (req, res) => {
   try {
     const { id } = req.params
@@ -69,6 +229,49 @@ router.delete('/:id', async (req, res) => {
   }
 })
 
+/**
+ * @openapi
+ * /user/{id}:
+ *  patch:
+ *    description: Update user by id
+ *    tags:
+ *      - user
+ *    parameters:
+ *      - name: id
+ *        in: path
+ *        description: User id
+ *        required: true
+ *        type: string
+ *    responses:
+ *      200:
+ *        description: Success
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                success:
+ *                  type: boolean
+ *                data:
+ *                  type: object
+ *                  properties:
+ *                    userUpdated:
+ *                      type: object
+ *                      properties:
+ *                        id:
+ *                          type: string
+ *                        name:
+ *                          type: string
+ *                        email:
+ *                          type: string
+ *                        password:
+ *                          type: string
+ *                        createdAt:
+ *                          type: date
+ *                        updatedAt:
+ *                          type: date
+ *
+ */
 router.patch('/:id', async (req, res) => {
   try {
     const { id } = req.params
